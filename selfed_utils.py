3aec63faa37b3835ca2e03ff135a16b2a78454d4
Antonov Nikita <kogemrka@gmail.com>
2015-05-24 18:12:33 +0300

    Add some script for dumping history to mongo

---
+++ selfed_utils.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+
+from slacker import Slacker
+from configparser import ConfigParser
+from pymongo import MongoClient
+import sys
+import json
+
+
+def test_api():
+    slack.api.test()
+    slack.auth.test()
+
+
+def join_all_channels():
+    # Note: this isn't going to work with bot users
+    response = slack.channels.list()
+    for channel in response.body['channels']:
+        if not channel['is_member']:
+            slack.channels.join(channel['name'])
+
+
+def store_history(chan_id):
+    # Todo: add some cheking for overwriting here
+    def add_chan_id(d):
+        d['channel_id'] = chan_id
+        return d
+
+    response = None
+    latest = None
+    collection = db['history']
+    while response is None or response.body["has_more"]:
+        response = slack.channels.history(chan_id, latest=latest)
+        messages = response.body['messages']
+        latest = messages[-1]['ts']
+        collection.insert_many(list(map(add_chan_id, messages)))
+
+
+def store_all_history():
+    collection = db['channels']
+    response = slack.channels.list()
+    channels = response.body['channels']
+    collection.insert_many(
+        list(map(lambda d: {'id': d['id'], 'name': d['name']}, channels)))
+    for channel in channels:
+        store_history(channel['id'])
+
+
+def update_history():
+    # Todo: to be implemented
+    pass
+
+commands = {
+    'test': test_api,
+    'join_all': join_all_channels,
+    'dump': store_all_history,
+    'update': update_history
+}
+
+# Todo: be aware of timeouts
+if __name__ == '__main__':
+    config = ConfigParser()
+    config.read('config.ini')
+    slack = Slacker(config['slack']['token'])
+    db_client = MongoClient(config['mongo']['URI'])
+    db = db_client[config['mongo']['database']]
+
+    if sys.argv[1] not in commands.keys():
+        print("Incorrect command")
+    else:
+        commands[sys.argv[1]]()
